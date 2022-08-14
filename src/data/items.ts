86638f040baf3ccfe3cf028f112df010128dcaf8
Eduardo <educonts08@gmail.com>
2022-08-14 19:22:37 -0300

    items.ts updated

--- src/data/items.ts
+++ src/data/items.ts
@@ -1,5 +1,14 @@
 export const items = [
   {
     date: new Date(2022, 7, 14), category: 'food', title: 'McDonalds', value: 32.12
+  },
+  {
+    date: new Date(2022, 7, 14), category: 'food', title: 'Burguer King', value: 28
+  },
+  {
+    date: new Date(2022, 7, 16), category: 'rent', title: 'Aluguel Apto', value: 2300
+  },
+  {
+    date: new Date(2022, 8, 18), category: 'salary', title: 'Salário ACME', value: 4500
   }
 ];
