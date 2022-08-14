59ed0729c120fbd05fceb066b98c81c583a98898
Eduardo <educonts08@gmail.com>
2022-08-14 19:47:21 -0300

    dateFilter.ts created from helpers and useState in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
+import { useState } from 'react'
+
 import { Container, Header, HeaderText, Body } from './App.styles';
+import { Item } from './types/Item'
+import { items } from './data/items'
+import { Category } from './types/Category'
+import { categories } from './data/categories'
+
+import { getCurrentMonth } from './helpers/dateFilter';
 
 const App = () => {
+  const [list, setList] = useState(items)
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth())
+
   return (
     <Container>
       <Header>
